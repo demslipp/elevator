7f5ff736c59b1e6d2399efcf3af6ba7fcd62325a
Test1 <jk1998@yandex.ru>
2019-10-15 14:31:43 +0300

    1

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main ( String[] args ) {
         ArrayList<Person> awaitables = new ArrayList<>();
         Runnable home = new Generator(awaitables);
         Runnable manager = new Manager(awaitables);
